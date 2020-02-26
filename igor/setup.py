a11a8f2127cad8ebc844106218e8bc22bbb8c2b3
cezary986 <cezary.maszczyk@gmail.com>
2020-02-26 20:12:55 +0100

    Create setup.py

---
+++ igor/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="igor-server",
+    version="0.0.1",
+    author="Cezary Maszczyk",
+    author_email="cezary.maszczyk@gmail.com",
+    description="Simple solution fo integrating python and modern web-based desktop apps e.g build using Elector",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cezary986/igor",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
